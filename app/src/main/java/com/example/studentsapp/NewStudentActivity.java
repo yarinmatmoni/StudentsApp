a09177de0f325218c03613a777a56e0db42c5c70
Yarin Matmoni <yarinmatmoni@cs.colman.ac.il>
2021-11-26 17:37:18 +0200

    details function

--- app/src/main/java/com/example/studentsapp/NewStudentActivity.java
+++ app/src/main/java/com/example/studentsapp/NewStudentActivity.java
@@ -3,12 +3,23 @@ package com.example.studentsapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import com.example.studentsapp.model.Model;
+import com.example.studentsapp.model.Student;
+
+import java.util.List;
 
 public class NewStudentActivity extends AppCompatActivity {
     Button cancel_btn;
     Button saveStudent_btn;
+    EditText nameStudent,idStudent,phoneStudent,addressStudent;
+    CheckBox cbStudent;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,6 +28,13 @@ public class NewStudentActivity extends AppCompatActivity {
 
         cancel_btn=findViewById(R.id.newStudent_can_btn);
         saveStudent_btn=findViewById(R.id.newStudent_save_btn);
+
+        nameStudent = findViewById(R.id.newStudent_name_pt);
+        idStudent = findViewById(R.id.newStudent_id_pt);
+        phoneStudent = findViewById(R.id.newStudent_phone_pt);
+        addressStudent = findViewById(R.id.newStudent_address_pt);
+        cbStudent = findViewById(R.id.newStudent_cb);
+
 
         cancel_btn.setOnClickListener(new View.OnClickListener() {
             @Override
--- app/src/main/java/com/example/studentsapp/StudentDetailsActivity.java
+++ app/src/main/java/com/example/studentsapp/StudentDetailsActivity.java
@@ -6,21 +6,45 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.TextView;
+
+import com.example.studentsapp.model.Model;
+import com.example.studentsapp.model.Student;
+
+import java.util.List;
 
 public class StudentDetailsActivity extends AppCompatActivity {
-
-    Button backToList_btn;
-    Button editStudent_btn;
-
+    List<Student> data;
+    Button backToList_btn,editStudent_btn;
+    TextView idStudent_d,nameStudent_d,phoneStudent_d,addressStudent_d;
+    CheckBox cbStudent_d;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_student_details);
 
+        data = Model.instance.getAllStudents();
         Intent intent_edit = new Intent(this,EditStudentActivity.class);
 
         backToList_btn=findViewById(R.id.studentDetails_back_btn);
         editStudent_btn=findViewById(R.id.studentDetails_edit_btn);
+
+        idStudent_d = findViewById(R.id.studentDatails_id_tv);
+        nameStudent_d = findViewById(R.id.studentDatails_name_tv);
+        phoneStudent_d = findViewById(R.id.studentDatails_phone_tv);
+        addressStudent_d = findViewById(R.id.studentDatails_address_tv);
+        cbStudent_d = findViewById(R.id.studentDetails_cb);
+
+        Bundle extras = getIntent().getExtras();
+        if(extras != null){
+            int posStudent = extras.getInt("posS");
+            idStudent_d.setText("ID : " + data.get(posStudent).getId());
+            nameStudent_d.setText("Name : " + data.get(posStudent).getName());
+            phoneStudent_d.setText("Phone : " + data.get(posStudent).getPhone());
+            addressStudent_d.setText("Address : " + data.get(posStudent).getAddress());
+            cbStudent_d.setChecked(data.get(posStudent).isCheck());
+        }
 
         backToList_btn.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -36,4 +60,5 @@ public class StudentDetailsActivity extends AppCompatActivity {
             }
         });
     }
+
 }
